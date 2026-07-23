8fef36258f24b0413a8f5ee4ae1390a10879d5e4
Mohit <vsmohit16nov@gmail.com>
2026-07-23 05:46:10 +0530

    feat: add SGPA calculation to Semester

--- models/semester.py
+++ models/semester.py
@@ -12,3 +12,22 @@ class Semester:
 
         self.subjects.append(new_subject)
 
+    def get_total_credits(self):
+        total_credits = 0
+        for subject in self.subjects:
+            total_credits += subject.credits
+
+        return total_credits
+
+    def get_total_weighted_points(self):
+        total_weighted_points = 0
+        for subject in self.subjects:
+            total_weighted_points += subject.credits * subject.get_grade_point()
+
+        return total_weighted_points
+
+    def calculate_sgpa(self):
+        if not self.subjects:
+            raise ValueError(
+                'Cannot calculate SGPA for a semester with no subjects.')
+        return round(self.get_total_weighted_points()/self.get_total_credits(), 2)
