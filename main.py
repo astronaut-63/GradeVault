b04be364d0cb06f4d5d6f45ed17d9a40a962fbcb
Mohit <vsmohit16nov@gmail.com>
2026-07-23 17:36:41 +0530

    feat: add student and semester views

--- main.py
+++ main.py
@@ -2,14 +2,16 @@ from models.subject import Subject
 from models.semester import Semester
 from models.student import Student
 
+LINE = '-'*40
+
 
 def menu():
     student = None
 
     while True:
-        print('1. Create Student\n2. Add Semester\n3. Add Subject\n4. Exit')
-        choice = int(input('Enter your choice: '))
+        print('1. Create Student\n2. Add Semester\n3. Add Subject\n4. View Student\n5. View Semester\n6. Exit')
         try:
+            choice = int(input('Enter your choice: '))
             if choice == 1:
                 name = input('Enter the name: ')
                 roll_number = input('Enter the roll number: ')
@@ -44,12 +46,43 @@ def menu():
                 except ValueError as e:
                     print(e)
             elif choice == 4:
+                try:
+                    if student is None:
+                        raise ValueError('Create a student first.')
+                    print(LINE)
+                    print(f'{'Name':<20}: {student.name}')
+                    print(f'{'Roll Number':<20}: {student.roll_number}')
+                    print()
+                    print(f'{'Semesters Completed':<20}: {len(student.semesters)}')
+                    print(f'{'CGPA':<20}: {student.calculate_cgpa():.2f}')
+                    print(LINE)
+                except ValueError as e:
+                    print(e)
+            elif choice == 5:
+                if student is None:
+                    raise ValueError('No student found.')
+                elif not student.semesters:
+                    raise ValueError('Semester not found.')
+                semester_asked = int(input('Semester number: '))
+                semester_to_display = student.find_semester(semester_asked)
+                print(f'{'Semester':<20}: {semester_to_display.number}')
+                print(f'{'SGPA':<20}: {semester_to_display.calculate_sgpa()}')
+                print(f'{'Credits':<20}: {semester_to_display.get_total_credits()}')
+                print()
+                print('Subjects')
+                print(LINE)
+                print(f'{'Subject':<30}{'Cr':<6}{'Marks':<8}{'Grade':<8}{'GP':<4}')
+                for subject in semester_to_display.subjects:
+                    print(
+                        f'{subject.name:<30}{subject.credits:<6}{subject.obtained_marks:<8}{subject.get_grade():<8}{subject.get_grade_point():<4}')
+                print(LINE)
+            elif choice == 6:
                 print('Thank you!')
                 break
             else:
-                print('Invalid choice')
-        except ValueError:
-            print('Enter a valid choice.')
+                raise ValueError('Invalid choice.')
+        except ValueError as e:
+            print(e)
 
 
 menu()
