4ca2dfde1f264aafa4f7189bee98ad9ef7f0e2c8
Mohit <vsmohit16nov@gmail.com>
2026-07-23 07:29:47 +0530

    feat: add CGPA calculation to Student

--- models/student.py
+++ models/student.py
@@ -4,12 +4,12 @@ class Student:
         self.semesters = []
         self.name = name
         self.roll_number = roll_number
-        
+
     def add_semester(self, new_semester):
         for existing_semester in self.semesters:
             if new_semester.number == existing_semester.number:
                 raise ValueError('This semester already exists!')
-            
+
         self.semesters.append(new_semester)
 
     def find_semester(self, number):
@@ -20,4 +20,15 @@ class Student:
                 return semester
             if not found:
                 raise ValueError('Semester not found!')
-    
+
+    def calculate_cgpa(self):
+        numerator = 0
+        denominator = 0
+        for semester in self.semesters:
+            credits = semester.get_total_credits()
+            numerator += semester.calculate_sgpa() * credits
+            denominator += credits
+        if denominator == 0:
+            raise ValueError(
+                'Cannot calculate CGPA because no subjects have been added.')
+        return numerator/denominator
