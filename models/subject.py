190cea3c028c9af0fde874d453b7ed063f1efac5
Mohit <vsmohit16nov@gmail.com>
2026-07-23 22:40:35 +0530

    refactor: reuse validation in Subject update methods

--- models/subject.py
+++ models/subject.py
@@ -5,18 +5,27 @@ GRADE_POINTS = {'O': 10, 'A+': 9, 'A': 8,
 class Subject:
 
     def __init__(self, name, credits, obtained_marks):
-        if not name.strip():
-            raise ValueError('Enter a valid name.')
+        self._validate_name(name)
 
-        if credits <= 0:
-            raise ValueError('Enter valid credits.')
+        self._validate_credits(credits)
 
-        if not 0 <= obtained_marks <= 100:
-            raise ValueError('Enter valid marks.')
+        self._validate_obtained_marks(obtained_marks)
 
         self.name = name
         self.credits = credits
         self.obtained_marks = obtained_marks
+
+    def _validate_credits(self, credits):
+        if credits < 0:
+            raise ValueError('Enter valid credits.')
+
+    def _validate_name(self, name):
+        if not name.strip():
+            raise ValueError('Enter a valid name.')
+
+    def _validate_obtained_marks(self, obtained_marks):
+        if not 0 <= obtained_marks <= 100:
+            raise ValueError('Enter valid marks.')
 
     def get_grade(self):
         if 90 <= self.obtained_marks <= 100:
@@ -39,3 +48,15 @@ class Subject:
     def get_grade_point(self):
         grade = self.get_grade()
         return GRADE_POINTS.get(grade)
+
+    def update_name(self, new_name):
+        self._validate_name(new_name)
+        self.name = new_name
+
+    def update_credits(self, new_credits):
+        self._validate_credits(new_credits)
+        self.credits = new_credits
+
+    def update_obtained_marks(self, new_obtained_marks):
+        self._validate_obtained_marks(new_obtained_marks)
+        self.obtained_marks = new_obtained_marks
